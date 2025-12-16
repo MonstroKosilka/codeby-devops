b3c405b92b12c92d005f495945c2be16c91e4349
MonstroKosilka <94645775+MonstroKosilka@users.noreply.github.com>
2025-12-16 16:42:13 +0300

    Изменения внесны в Hello Jenkins! APP и Hello Devops! APP

--- lesson26/app-Devops/src/main/java/com/mycompany/app/App.java
+++ lesson26/app-Devops/src/main/java/com/mycompany/app/App.java
@@ -1,7 +1,7 @@
 package com.mycompany.app;
 
 /**
- * Hello world! Проверка 1
+ * Hello Devops!
  */
 public class App {
 
--- lesson26/app-Jenkins/src/main/java/com/mycompany/app/App.java
+++ lesson26/app-Jenkins/src/main/java/com/mycompany/app/App.java
@@ -1,7 +1,7 @@
 package com.mycompany.app;
 
 /**
- * Hello world!Проверка 2
+ * Hello Jenkins!
  */
 public class App {
 
